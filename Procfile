851b934eaaed59c84ac96da2d9c678f5a88b571e
Kwes <onesimusgravessampson1@gmail.com>
2022-09-08 21:47:06 +0000

    fix_axion1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: echo "heree" && npm i axios -g && npm run start:prod
+web: npm run start:prod
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:pass123@localhost:5435/ejara?schema=public',
+          url: 'postgres://ehrmlgmwydalqd:43f5b7112334a07b73791be6c9037e69f7368600b42d7fab4a7bcd86d8ea491c@ec2-3-208-79-113.compute-1.amazonaws.com:5432/db32i51q8v86qm',
         },
       },
     });
