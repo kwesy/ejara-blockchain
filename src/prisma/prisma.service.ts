56cb7002ccb1e440bf0e0606ebf7606532700d7e
Kwes <onesimusgravessampson1@gmail.com>
2022-09-08 23:09:21 +0000

    fixed issues

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ export class PrismaService extends PrismaClient {
       datasources: {
         db: {
           url:
-            process.env.DATABASE_URL ||
+            process.env.HEROKU_POSTGRESQL_YELLOW_URL ||
             'postgres://ehrmlgmwydalqd:43f5b7112334a07b73791be6c9037e69f7368600b42d7fab4a7bcd86d8ea491c@ec2-3-208-79-113.compute-1.amazonaws.com:5432/db32i51q8v86qm',
         },
       },
